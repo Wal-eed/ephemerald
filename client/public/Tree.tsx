699bb66efe1a97c2543c59f11345041abad88b04
Max Phillips <max@maxphillipsdev.com>
2021-08-28 19:17:00 +1000

    I hate trees

--- client/public/Tree.tsx
+++ client/public/Tree.tsx
@@ -6,39 +6,45 @@ source: https://sketchfab.com/3d-models/pine-tree-e52769d653cd4e52a4acff3041961e
 title: Pine tree
 */
 
-import * as THREE from 'three'
-import React, { useRef } from 'react'
-import { useGLTF } from '@react-three/drei'
-import { GLTF } from 'three/examples/jsm/loaders/GLTFLoader'
+import * as THREE from "three";
+import React, { useRef } from "react";
+import { useGLTF } from "@react-three/drei";
+import { GLTF } from "three/examples/jsm/loaders/GLTFLoader";
 
 type GLTFResult = GLTF & {
   nodes: {
-    Leaves001_Leavs_0: THREE.Mesh
-    Trank001_Trank_0: THREE.Mesh
-  }
+    Leaves001_Leavs_0: THREE.Mesh;
+    Trank001_Trank_0: THREE.Mesh;
+  };
   materials: {
-    Leavs: THREE.MeshStandardMaterial
-    Trank: THREE.MeshStandardMaterial
-  }
-}
+    Leavs: THREE.MeshStandardMaterial;
+    Trank: THREE.MeshStandardMaterial;
+  };
+};
 
-export default function Model(props: JSX.IntrinsicElements['group']) {
-  const group = useRef<THREE.Group>()
-  const { nodes, materials } = useGLTF('/tree.gltf') as GLTFResult
+export default function Model(props: JSX.IntrinsicElements["group"]) {
+  const group = useRef<THREE.Group>();
+  const { nodes, materials } = useGLTF("/tree.gltf") as GLTFResult;
   return (
     <group ref={group} {...props} dispose={null}>
       <group rotation={[-Math.PI / 2, 0, 0]}>
         <group rotation={[Math.PI / 2, 0, 0]}>
-          <group rotation={[-Math.PI / 2, 0, 0]} scale={[100, 100, 100]}>
-            <mesh geometry={nodes.Leaves001_Leavs_0.geometry} material={materials.Leavs} />
+          <group rotation={[-Math.PI / 2, 0, 0]} scale={[1, 1, 1]}>
+            <mesh
+              geometry={nodes.Leaves001_Leavs_0.geometry}
+              material={materials.Leavs}
+            />
           </group>
-          <group rotation={[-Math.PI / 2, 0, 0]} scale={[100, 100, 100]}>
-            <mesh geometry={nodes.Trank001_Trank_0.geometry} material={materials.Trank} />
+          <group rotation={[-Math.PI / 2, 0, 0]} scale={[1, 1, 1]}>
+            <mesh
+              geometry={nodes.Trank001_Trank_0.geometry}
+              material={materials.Trank}
+            />
           </group>
         </group>
       </group>
     </group>
-  )
+  );
 }
 
-useGLTF.preload('/tree.gltf')
+useGLTF.preload("/tree.gltf");
--- client/src/pages/Memorabilia.jsx
+++ client/src/pages/Memorabilia.jsx
@@ -7,17 +7,28 @@ import Model from "src/components/Tree";
 const Memorabilia = () => {
   return (
     <VStack p="1rem">
-      <Box w="100%" borderWidth="1px" borderRadius="lg" overflow="hidden">
-        <Canvas camera={{ position: [-100, 200, -100], fov: 90 }}>
-          <ambientLight intensity={0.5} />
-          <OrbitControls />
-          <Suspense fallback={null}>
-            <Model />
-          </Suspense>
-        </Canvas>
+      <Text fontSize="4xl">Badges</Text>
+      <Box w="100%" borderWidth="1px" borderRadius="1rem">
+        <Box w="100%" h="md">
+          <Canvas camera={{ position: [100, 100, 100], fov: 90 }}>
+            <ambientLight intensity={0.5} />
+            <OrbitControls />
+            <Suspense fallback={null}>
+              <Model />
+            </Suspense>
+          </Canvas>
+        </Box>
         <Divider />
-        <Text fontSize="2xl" as="strong" p="6">
+        <Text p="1rem" fontSize="2xl" as="strong">
           Tree Badge
+        </Text>
+        <Text paddingInline="1rem">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti sunt
+          dolores eligendi ullam veritatis, debitis deserunt, distinctio
+          officiis esse eos voluptatem ex odit labore odio illum hic aliquid!
+          Tempora, consequuntur quas iure deleniti vitae sed expedita esse
+          consectetur autem delectus quod nam eaque praesentium, cum temporibus
+          ipsam, eius dolore ea?
         </Text>
       </Box>
     </VStack>
