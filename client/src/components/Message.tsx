b1fd885215adee44db424b699b61c23b8af1c6e3
Max Phillips <max@maxphillipsdev.com>
2021-08-28 14:11:15 +1000

    Updated messages

--- client/src/components/Message.tsx
+++ client/src/components/Message.tsx
@@ -1,10 +1,20 @@
-import { Box, HStack, Text, VStack } from '@chakra-ui/react'
+import { Avatar, Box, HStack, Text, VStack } from '@chakra-ui/react'
 import React from 'react'
 
-export const Message: React.FC = ({ children}) => {
+
+interface Props {
+  name: string;
+  time: string;
+}
+
+export const Message: React.FC<Props> = ({ name, time, children}) => {
   return (
-    <HStack>
-{children}
+    <HStack w="100%"  paddingInline="0.5rem">
+      <Avatar name={ name}/>
+      <VStack spacing={0} justifySelf="start">
+        <HStack alignSelf="start"><Text as="strong">{name}</Text><Text as="i">{time}</Text></HStack>
+        <Text justifySelf="start" textAlign="start">{children}</Text>
+      </VStack>
     </HStack>
   )
 }
--- client/src/pages/Messages.tsx
+++ client/src/pages/Messages.tsx
@@ -1,5 +1,6 @@
-import {  Box, HStack, VStack } from "@chakra-ui/react";
+import {  Box, Divider, HStack, StackDivider, VStack } from "@chakra-ui/react";
 import React from "react";
+import { Chip } from "src/components/Chip";
 import { Message } from "src/components/Message";
 
 interface Props {}
@@ -9,16 +10,25 @@ const Messages: React.FC<Props> = () => {
 
   return (
     <VStack>
-      <Box w="100%" h="48" backgroundColor="ghostwhite">
-        <HStack overflowX="scroll" overflowY="hidden">
-        </HStack>
-      </Box>
-    <VStack>
-      <Message>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, commodi.</Message>
-      <Message>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, commodi.</Message>
-      <Message>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, commodi.</Message>
+      <HStack w="100%" p="0.5rem" mt="0.5rem" overflowX="auto" overflowY="hidden" whiteSpace="nowrap" wrap="nowrap">
+        <Chip>Channel 1</Chip>
+        <Chip>Cools channel</Chip>
+        <Chip>uncool channel</Chip>
+        <Chip>lmao</Chip>
+        <Chip>join 4 free vbucks</Chip>
+        <Chip>Channel 1</Chip>
+        <Chip>Cools channel</Chip>
+        <Chip>uncool channel</Chip>
+        <Chip>lmao</Chip>
+        <Chip>join 4 free vbucks</Chip>
+      </HStack>
+      <Divider />
+      <VStack divider={<StackDivider borderColor="gray.100" opacity="30%" />} >
+        <Message name="John Smith" time="2:07am">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus, ducimus!</Message>
+        <Message name="Bob Bobson" time="2:07am">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus, ducimus!</Message>
+        <Message name="John Smith" time="2:07am">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus, ducimus!</Message>
       </VStack>
-      </VStack>
+    </VStack>
     );
 };
 
