2b6aa37250c7c9a618537dc2f39394848f24b1e2
Tymotex <timzhang3@gmail.com>
2021-08-28 14:44:53 +1000

    Added response and data rendering for question thread

--- client/src/components/Questions/QuestionThread.tsx
+++ client/src/components/Questions/QuestionThread.tsx
@@ -1,58 +1,30 @@
-import { Box } from "@chakra-ui/layout";
 import { Avatar } from "@chakra-ui/avatar";
+import { Box } from "@chakra-ui/layout";
 import React from "react";
-import jas from "../../assets/jas.png";
-import elon from "../../assets/elon.png";
 import Card from "src/components/Card/Card";
+import { Question } from "src/pages/QA";
+import QuestionResponse from "./QuestionResponse";
 
-interface Props {}
+interface Props {
+    question: Question;
+}
 
-const QuestionThread: React.FC<Props> = () => {
+const QuestionThread: React.FC<Props> = ({ question }) => {
     return (
         <Card>
-            <Avatar name="AVATAR" size="lg" src={jas} />
+            <Avatar name="AVATAR" size="lg" src={question.authorAvatar} />
+            {/* <p>{question.authorName}</p> */}
             <Box
                 style={{
                     marginTop: 14,
                     marginBottom: 14,
                 }}
             >
-                <em>
-                    "Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Architecto odit sit accusamus ut reprehenderit provident
-                    nesciunt explicabo quo. Optio et iusto animi quasi voluptas
-                    officiis alias voluptate accusantium omnis beatae?"
-                </em>
+                <em>"{question.question}"</em>
             </Box>
-            <Box
-                style={{
-                    background: "#f0f0f0",
-                    padding: 14,
-                    borderRadius: 10,
-                    position: "relative",
-                }}
-            >
-                <Avatar
-                    name="Avatar"
-                    src={elon}
-                    size="md"
-                    style={{
-                        position: "absolute",
-                        left: "2%",
-                    }}
-                ></Avatar>
-                <Box
-                    style={{
-                        background: "white",
-                        height: "50px",
-                        marginLeft: "70px",
-                        textAlign: "left",
-                        padding: 14,
-                    }}
-                >
-                    A: yes
-                </Box>
-            </Box>
+            {question.responses.map((response) => (
+                <QuestionResponse response={response} />
+            ))}
         </Card>
     );
 };
